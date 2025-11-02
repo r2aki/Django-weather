dd0fa418dd532f028e053fa3a553d69dc9852cfc
Ivan <sir239@yandex.ru>
2025-11-02 11:51:55 +0300

    form, model and admin for Cities

--- WeatherApp/weather/admin.py
+++ WeatherApp/weather/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import City
 
-# Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
